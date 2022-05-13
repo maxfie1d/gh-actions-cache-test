af282bcd1b74600458dbe10f79174ceaaf2748e3
maxfie1d <7913793+maxfie1d@users.noreply.github.com>
2022-05-13 21:31:26 +0900

    Update MainActivity.kt

--- app/src/main/java/com/github/maxfie1d/with/gh_actions_cache_test/MainActivity.kt
+++ app/src/main/java/com/github/maxfie1d/with/gh_actions_cache_test/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Hello Android 7")
+                    Greeting("Hello Android 8")
                 }
             }
         }
